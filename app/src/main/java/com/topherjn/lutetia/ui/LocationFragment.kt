91eebb896eed0af7bcb8d84fa8dbb1bfdc55f301
Christopher Nicholson <nicholsonc@gmail.com>
2022-05-09 15:17:44 -0400

    location logic added

--- app/src/main/java/com/topherjn/lutetia/ui/LocationFragment.kt
+++ app/src/main/java/com/topherjn/lutetia/ui/LocationFragment.kt
@@ -44,21 +44,109 @@ class LocationFragment : Fragment() {
         // change title in title bar
         (activity as AppCompatActivity).supportActionBar?.title = "Arrondissement"
 
-        val action = LocationFragmentDirections.actionLocationFragmentToSiteListFragment(4)
-
-        binding.arrondissementTextView.text = "88"
-        binding.arrondissementTextView.setOnClickListener { view -> view.findNavController().navigate(action)}
-
+        startLocationUpdates()
     }
 
+    private fun startLocationUpdates() {
 
+        client = LocationServices.getFusedLocationProviderClient(requireContext())
 
+        locationRequest = LocationRequest.create()
+        locationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest!!.interval = 5000
+        locationRequest!!.fastestInterval = 1000
+        locationRequest!!.isWaitForAccurateLocation = true
+        locationRequest!!.smallestDisplacement = 0f
 
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    Log.d("STARTLOCATION","location gotten")
+                    updateLocationTextBox(location)
+                }
+            }
+        }
 
+        Log.d("STARTLOCATION","locationcallback")
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), PERMISSION_REQUEST_CODE
+            )
 
+            return
+        }
+        Log.d("STARTLOCATION","return called")
+        client!!.requestLocationUpdates(locationRequest!!, locationCallback!!, Looper.getMainLooper())
 
+        client!!.lastLocation
+            .addOnSuccessListener { location: Location? -> updateLocationTextBox(location!!) }
 
+        isTracking = true
+    }
 
+    private fun stopLocationUpdates() {
+        if(isTracking) {
+            client!!.removeLocationUpdates(locationCallback!!)
+            client = null
+            isTracking = false
+        }
+    }
 
+    private fun updateLocationTextBox(lastLocation: Location) {
+        //Toast.makeText(requireContext(), "updateLocationTextBox", Toast.LENGTH_SHORT).show()
+        val geocoder = Geocoder(context)
 
+        try {
+            val addresses =
+                geocoder.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1)
+            var postalCode = addresses[0].postalCode
+
+            Log.d("STARTLOCATION",postalCode.toString())
+            //Toast.makeText(requireContext(), postalCode.toString(), Toast.LENGTH_LONG).show()
+            if (postalCode.length > 1) {
+                postalCode = postalCode.substring(postalCode.length - 2)
+                if (postalCode[0] == '0') postalCode = postalCode.substring(postalCode.length - 1)
+            }
+            var arrondissement = postalCode.toInt()
+            if(arrondissement > 20) arrondissement = arrondissement.mod(20) + 1
+
+            binding.arrondissementTextView.text = arrondissement.toString()
+            binding.arrondissementTextView.isEnabled = true
+
+            val action = LocationFragmentDirections.actionLocationFragmentToSiteListFragment(arrondissement)
+            binding.arrondissementTextView.setOnClickListener { view -> view.findNavController().navigate(action)}
+
+        } catch (e: Exception) {
+            //Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+            binding.arrondissementTextView.text = e.message
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("STARTLOCATION","onResume called")
+        if(!isTracking)
+            startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        stopLocationUpdates()
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 200
+    }
 }
